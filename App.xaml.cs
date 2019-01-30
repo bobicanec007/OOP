7dca639361031c2b1e58f79f9961718c42e064a1
bobicanec007 <bobicanec007@gmail.com>
2019-01-30 22:11:10 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Kalk mw = new Kalk();
+            mw.Show();
+        }
+    }
+}
