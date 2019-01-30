3939913dde5ba01f2ebc276a2391c02dad7ac7d5
bobicanec007 <bobicanec007@gmail.com>
2019-01-30 21:58:16 +0100

    Add files via upload

---
+++ Razlomak.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Razlomak1
+{
+
+    public class Razlomak  : IComparable<Razlomak>, IEquatable<Razlomak>
+    {
+        private int brojnik, nazivnik;
+
+        // konstruktor
+        public Razlomak(int brojnik, int nazivnik)
+        {
+            this.brojnik = brojnik;
+            this.nazivnik = nazivnik;
+        }
+
+        public void ZadajBrojnik(int brojac)
+        {
+
+            this.brojnik = brojnik;
+
+        }
+        public void ZadajNazivnik(int nazivnik)
+        {
+            this.nazivnik = nazivnik;
+
+        }
+
+        public int VratiBrojnik()
+        {
+            return brojnik;
+
+        }
+        public int VratiNazivnik()
+        {
+            return nazivnik;
+
+        }
+        public float PretvoriUFloat()
+
+        {
+            return (float)brojnik / nazivnik;
+        }
+
+        public bool Ispravan()
+        {
+            return nazivnik != 0;
+        }
+        private int nzm(int a, int b)
+
+        {
+            int temp;
+            while (b != 0)
+            {
+                temp = b;
+                b = a % b;
+               a = temp;
+            }
+            return a;
+
+        }
+        public void SkratiRazlomak()
+        {
+            if (brojnik < nazivnik)
+            {
+                int mjera = nzm(nazivnik, brojnik);
+                brojnik = brojnik /mjera;
+                nazivnik = nazivnik / mjera;
+            }
+            else
+            {
+                int mjera = nzm(brojnik, nazivnik);
+                brojnik /= mjera;
+                nazivnik /= mjera; 
+            }
+
+            }
+
+        public int CompareTo(Razlomak other)
+        {
+           //throw new NotImplementedException();
+          if (PretvoriUFloat()>other.PretvoriUFloat())
+            return 1;
+
+            else if( PretvoriUFloat() < other.PretvoriUFloat())
+                    return -1;
+            else
+            return 0;
+        }
+        //kolkoovij
+        public bool Equals(Razlomak other)
+        {
+            //throw new NotImplementedException();
+            return brojnik == other.VratiBrojnik() &&
+                nazivnik == other.VratiNazivnik();
+            }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return this.Equals((Razlomak)obj);
+        //}
+    }
+    }
+    
---
+++ Vrijeme.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba4_Vrijeme.Logika
+{
+    public class Vrijeme
+    {
+        // privatna varijabla koja zapisuje stanje vrremena
+        private int sekundi;
+
+        // konstruktori
+        public Vrijeme()
+        {
+            sekundi = 0;
+        }
+
+        public Vrijeme(int sek)
+        {
+            sekundi = sek;
+        }
+
+        public Vrijeme(string hhmmss)
+        {
+            //sekundi =
+            //    int.Parse(hhmmss.Split(':')[0]) * 60 * 60 +
+            //    int.Parse(hhmmss.Split(':')[1]) * 60 +
+            //    int.Parse(hhmmss.Split(':')[2]);
+            string[] hms = hhmmss.Split(':');
+            sekundi =
+                int.Parse(hms[0]) * 60 * 60 +
+                int.Parse(hms[1]) * 60 +
+                int.Parse(hms[2]);
+        }
+
+        // vrati vrijeme kao string
+        public string VratiString()
+        {
+            int sati = sekundi / 3600;
+            int minuta = (sekundi % 3600) / 60;
+            int sek = sekundi % 60;
+            return string.Format("Vrijeme {0}s = {1} sati, {2} minuta i {3} sekundi",
+                sekundi, sati, minuta, sek);
+        }
+
+        // zadaj metode
+        public void ZadajSate(double sati)
+        {
+            sekundi = (int)(sati * 3600);
+        }
+
+        public void ZadajMinte(double minuta)
+        {
+            sekundi = (int)(minuta * 60);
+        }
+
+        public void ZadajSekunde(int sek)
+        {
+            sekundi = sek;
+        }
+
+        // vrati metoda
+        public double VratiSate()
+        {
+            return (double)sekundi / 3600;
+        }
+
+        public double VratiMinute()
+        {
+            return (double)sekundi / 60;
+        }
+
+        public int VratiSekunde()
+        {
+            return sekundi;
+        }
+
+        // dodaj i oduzmi vremena
+        public void DodajVrijeme(Vrijeme v)
+        {
+            sekundi += v.VratiSekunde();
+        }
+
+        public void OduzmiVrijeme(Vrijeme v)
+        {
+            sekundi -= v.VratiSekunde();
+        }
+    }
+}
