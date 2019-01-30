d3d39724da889876b6629365e16be0c555b273e0
bobicanec007 <bobicanec007@gmail.com>
2019-01-30 22:37:44 +0100

    Add files via upload

---
+++ StudentMeva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korisnici
+{
+    class StudentMeva : Student
+    {
+        private string fakultet = "MEV";
+
+        public new string PredstaviSe()
+        {
+            return String.Format("Ja sam student {0}, {1} godine studija {2}, zaporka mi je {3}",
+                korisnickoIme,godinaStudija,fakultet, zaporka);
+        }
+    }
+}
