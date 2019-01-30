f58db66b39279843388eadab9f422ddcdf4605c2
bobicanec007 <bobicanec007@gmail.com>
2019-01-30 22:12:42 +0100

    Add files via upload

---
+++ Korisnik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korisnici
+{
+    public class Korisnik
+    {
+        protected string korisnickoIme;
+        protected string zaporka;
+
+        public Korisnik()
+        {
+            korisnickoIme = "nedefinirano";
+            zaporka = "12345";
+            Console.WriteLine("Pozivam inicijalni konstruktor korisnika");
+        }
+        public void Zadaj(string  _korisnickoIme, string _zaporka)
+        {
+            korisnickoIme = _korisnickoIme;
+            zaporka = _zaporka;
+        }
+        public Korisnik(string _korisnickoIme, string _zaporka)
+        {
+            korisnickoIme = _korisnickoIme;
+            zaporka = _zaporka;
+        }
+        public virtual string PredstaviSe()
+        {
+            return String.Format("Ja sam korisnik {0} zaporka mi je {1}",
+                korisnickoIme, zaporka);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,97 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Korisnici
 {
-    public struct Knjiga
-    {
-        public double cijena;
-        public string tip;
-        public string autor;
-        public int kolicina;
-    }
-
     class Program
     {
-        private static List<Knjiga> knjige = new List<Knjiga>();
+        private static List<Korisnik> korisnici = new List<Korisnik>();
+
         static void Main(string[] args)
         {
-            Knjiga k1 = new Knjiga();
-            k1.tip = "Harry Potter i Kamen Mudraca";
-            k1.autor = "J.K.Rowling";
-            k1.cijena = 200;
-            k1.kolicina = 1;
-            knjige.Add(k1);
+            Korisnik marko = new Korisnik("marko@mev.hr", "5555");
+           //marko.Zadaj("marko@mev.hr","5555");
+            Console.WriteLine(marko.PredstaviSe());
 
+            Console.WriteLine();
+            Student ivan = new Student("iivan@mev.hr", "0000");
+            Console.WriteLine(ivan.PredstaviSe());
+            //ivan.Zadaj("iivan@mev.hr", "0000");
+            ivan.ZadajGodinuStudija(2);
+            Console.WriteLine(((Korisnik)ivan).PredstaviSe());
 
-            knjige.Add(new Knjiga
-            {
-                tip = "Pale sam na svijetu",
-                autor = "Mateo",
-                cijena = 100,
-                kolicina = 2,
-            });
+            Console.WriteLine();
+            Profesor pero = new Profesor();
+            pero.Zadaj("ppero@mev.hr", "1111");
+            pero.ZadajPredmete("OOP1, AISP, PAUP");
+            Console.WriteLine(pero.PredstaviSe());
 
-            knjige.Add(new Knjiga
-            {
-                tip = "Harry Potter i Plameni Pehar",
-                autor = "J.K.Rowling",
-                cijena = 200,
-                kolicina = 3,
-            });
+            Console.WriteLine();
+            StudentMeva ana = new StudentMeva();
+            Console.WriteLine(ana.PredstaviSe());
 
-            knjige.Add(new Knjiga
-            {
-                tip = "Zlocin i Kazna",
-                autor = "Ivan Bartolic",
-                cijena = 140,
-                kolicina = 2,
-            });
+            korisnici.Add(marko);
+            korisnici.Add(ivan);
+            korisnici.Add(pero);
 
-            knjige.Add(new Knjiga
+            Console.WriteLine("\nPopis korisnika:");
+            foreach(Korisnik k in korisnici)
             {
-                tip = "Crvenkapica",
-                autor = "Josip Strosmajer",
-                cijena = 160,
-                kolicina = 4,
-            });
-            double min = double.MaxValue;
-            foreach (Knjiga k in knjige)
-            {
-                if (k.cijena < min)
-                {
-                    min = k.cijena;
-                }
+                Console.WriteLine(k.PredstaviSe());
             }
-
-            double ukupnaCijenaKnjiga = 0;
-            int brojKnjiga = 0;
-            foreach (Knjiga k in knjige)
-            {
-                brojKnjiga++;
-                ukupnaCijenaKnjiga += k.cijena;
-            }
-
-            double ukupnaCijena = 0;
-            foreach (Knjiga k in knjige)
-            {
-                ukupnaCijena += k.kolicina * k.cijena;
-                Console.WriteLine("{0,-20}{1,-20}{2,10}{3,10}", k.tip, k.autor, k.cijena, k.kolicina);
-            }
-            Console.WriteLine("Ukopni iznos {0}", ukupnaCijena);
-            Console.WriteLine("Najeftinija knjiga {0}", min);
-            Console.WriteLine("Prosjecna cijena svih knjiga iznosi: {0}", ukupnaCijenaKnjiga / brojKnjiga);
+            Console.WriteLine();
         }
     }
 }
-
-
-//moramo napisati 10 knjiga u neku liste, ispiste prosjecnu cijenu artikla, 
-//napraviti strukturu sa par polja, list tip, tipa knjiga, dodati u tu listu 5knjiga, najeftinja knjiga je ta, prosjecna cijena je ta.
-//razredi nece biti na kolokviju-to danas.
-
-   //List<knjiga>
-   //3,4 knjige
-   //ukopni iznos
-   //najeftinije knjige
-   //prosjecnu cijenu
